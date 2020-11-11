6f2e3e66ec56a2d7d84d82414efcb451eab1a94d
Edoardo Mantovani <Baseband@cpan.org>
2020-11-11 08:43:30 +0100

    created inclusion file

---
+++ Ctxs.h
@@ -0,0 +1,17 @@
+#ifndef __CTXS_H
+#define __CTXS_H
+
+// Made by Edoardo Mantovani, 2020
+// import libraries
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+#ifdef _UAPI_LINUX_REBOOT_H
+  #include <linux/kernel.h>
+#endif
+
+#include "ppport.h"
+
+#endif /* __CTXS_H */
